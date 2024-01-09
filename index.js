2857ae3118bc47f8bf04dd7a3dfed22bef4486f0
Charles Olupot <olupotcharles@gmail.com>
2024-01-09 15:57:53 +0300

    Set navigation timeout

--- index.js
+++ index.js
@@ -49,7 +49,10 @@ class Webpage {
         await page.type("#j_password", password);
         console.log("Logging in");
         await page.click("#submit");
-        await page.waitForNavigation();
+        await page.waitForNavigation({
+            waitUntil: "networkidle0",
+            timeout: 0,
+        });
         console.log("Getting the page");
         await page.goto(`${url}/${additionalUrl}`, {
             waitUntil: "networkidle0",
